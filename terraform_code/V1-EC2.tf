6adccc16009be735af73b4169e0dcfb7be5b7e6d
chaitanyabr91 <chaitanyabr1990@gmail.com>
2024-02-09 15:56:13 +0530

    adding tf files

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+    region = "ap-south-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-0449c34f967dbf18a"
     instance_type = "t2.micro"
     key_name = "dpp"
+  
 }
